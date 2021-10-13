7ceb030fd4a2e283dacc5045a964562651b4c179
nerocui <nerocui@outlook.com>
2021-10-13 15:16:55 -0700

    added dev thingy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function initialize() {
     microsoftTeams.initialize(() => {}, [
-        "https://nerocui.github.io",
-    ]);
+        "https://nerocui.github.io/fhl-teams-vid",
+    ]);//change to https://fhl.local:3000 for local dev
     microsoftTeams.appInitialization.notifySuccess();
     registerHandlers();
 }
