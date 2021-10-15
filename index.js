fafcc9321066df09ea2a5d2d47e66e0a636a2022
nerocui <nerocui@outlook.com>
2021-10-14 21:34:40 -0700

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+function encode(nv12) {
+    var x = new Uint8ClampedArray(nv12);
+    let y_mat = new cv.Mat(16, 2, cv.CV_8UC1)
+    let u_mat = new cv.Mat(8, 1, cv.CV_8UC1)
+    let v_mat = new cv.Mat(8, 1, cv.CV_8UC1)
+    //16*2 + 8*1 + 8*1
+    for (var i = 0; i < 2* 16; i++) {
+        y_mat.data[i] = x[i];
+    }
+
+    for (var i = 32; i < x.length; i+=2) {
+        u_mat.data[i] = x[i];
+        v_mat.data[i] = x[i + 1];
+    }
+    let u_mat_2 = new cv.Mat();
+    cv.resize(u_mat, u_mat_2, new cv.Size(16, 2), 0, 0, cv.INTER_AREA);
+    let v_mat_2 = new cv.Mat();
+    cv.resize(v_mat, v_mat_2, new cv.Size(16, 2), 0, 0, cv.INTER_AREA);
+
+    let yuv_mat = new cv.Mat(16, 2, cv.CV_8UC3)
+    for (var i = 0; i < x.length; i++) {
+        if (i < 32)
+            yuv_mat.data[i] = y_mat.data[i];
+        else {
+            if (i < 32 + 32)
+                yuv_mat.data[i] = u_mat_2.data[i];
+            else
+                yuv_mat.data[i] = v_mat_2.data[i];
+        }
+    }
+    let dst = new cv.Mat();
+    cv.cvtColor(yuv_mat, dst, cv.COLOR_YUV2BGR, 0);
+    return dst;
+}
+
 function initialize() {
     microsoftTeams.initialize(() => {}, [
         "https://nerocui.github.io/fhl-teams-vid",
@@ -25,18 +60,10 @@ function effectParameterChanged(effectName) {
     console.log(`Parameter changed. ${effectName}`);
 }
 
-var savedAll = false;
 
 function videoFrameHandler(videoFrame, notifyVideoProcessed) {
     try {
-        if (!savedAll) {
-            localStorage.setItem("videoFrame.data", videoFrame.data);
-            localStorage.setItem("videoFrame.width", videoFrame.width);
-            localStorage.setItem("videoFrame.height", videoFrame.height);
-            localStorage.setItem("length", videoFrame.data.length);
-            savedAll = true;
-        }
-        const mat = cv.matFromImageData(videoFrame.data);
+        const mat = encode(videoFrame.data);
         console.log(mat);
     } catch (err) {
         console.log(err);
@@ -45,6 +72,6 @@ function videoFrameHandler(videoFrame, notifyVideoProcessed) {
     notifyVideoProcessed();
 }
 
-initialize();
-
-
+cv['onRuntimeInitialized'] = function() {
+    initialize();
+}
